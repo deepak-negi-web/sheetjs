d5712a82eb4e8de8726e87065cc72a721ae992ac
deepak-negi-web <st.deepak15@gmail.com>
2024-08-14 17:22:28 +0530

    fix: infinite loop due to hidden row in XLSX.stream.to_json

--- bits/97_node.js
+++ bits/97_node.js
@@ -107,7 +107,10 @@ function write_json_stream(sheet/*:Worksheet*/, opts/*:?Sheet2CSVOpts*/) {
 	R = r.s.r + offset;
 	stream._read = function() {
 		while(R <= r.e.r) {
-			if ((rowinfo[R-1]||{}).hidden) continue;
+			if ((rowinfo[R]||{}).hidden) {
+				++R;
+				continue;
+			};
 			var row = make_json_row(sheet, r, R, cols, header, hdr, dense, o);
 			++R;
 			if((row.isempty === false) || (header === 1 ? o.blankrows !== false : !!o.blankrows)) {
